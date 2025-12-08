f9ba243d638f87362010f6e4f60de3f6bc0a5427
Mirko Srbinovski <srbinovskimirko@gmail.com>
2025-12-09 09:41:50 +1000

    Initial commit

---
+++ test/mycdkapp.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Mycdkapp from '../lib/mycdkapp-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/mycdkapp-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Mycdkapp.MycdkappStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
